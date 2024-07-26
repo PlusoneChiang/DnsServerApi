cdc70cbc527cf2d35f1cbf65438f7e2d98134a2b
Plusone Chiang <plusone@appx.ocm.tw>
2024-07-26 16:17:44 +0800

    upgrade framework to net 8

--- src/Dns.WindowsDnsServer.Demo/Program.cs
+++ src/Dns.WindowsDnsServer.Demo/Program.cs
@@ -12,18 +12,25 @@ namespace Dns.WindowsDnsServer.Demo
 
             // connect to DNS server
             Write($"Connecting to {Environment.GetEnvironmentVariable("DnsServerAddress")}...");
-            using (var server = WindowsDnsServer.Connect(
-                Environment.GetEnvironmentVariable("DnsServerAddress"),
-                Environment.GetEnvironmentVariable("DnsServerAuthority"),
-                Environment.GetEnvironmentVariable("DnsServerUsername"),
-                Environment.GetEnvironmentVariable("DnsServerPassword")))
-            {
-                WriteLine(" [Done]");
+            //using (var server = WindowsDnsServer.Connect(
+            //    Environment.GetEnvironmentVariable("DnsServerAddress"),
+            //    Environment.GetEnvironmentVariable("DnsServerUsername"),
+            //    Environment.GetEnvironmentVariable("DnsServerPassword")))
+            //{
+            //    WriteLine(" [Done]");
 
-                DumpDnsServer(server);
+            //    DumpDnsServer(server);
 
-            }
-
+            //}
+            using var server = WindowsDnsServer.Connect("192.168.68.170", "ntlmdomain:DOMAIN", "administrator", "1qaz@WSX");
+            // using var server = WindowsDnsServer.Connect("192.168.68.170");
+            // server.CreateZone("appx.com", DnsZoneType.Primary, false, "plusnone");
+            // server.CreateZone("appx-dev.com", DnsZoneType.Primary, false, "plusnone");
+            // server.DeleteZone("appx.sample.com");
+            // server.GetZone("appx.com").CreateARecord("www.appx.com", TimeSpan.FromDays(365), "192.168.68.170");
+            // server.GetZone("appx.com").CreateCNAMERecord("sub.appx.com", TimeSpan.FromDays(365), "www.appx.com");
+            // server.GetZone("appx-dev.com").CreateCNAMERecord("sub.appx-dev.com", TimeSpan.FromDays(365), "www.appx-dev.com");
+            DumpDnsServer(server);
         }
 
         #region Console Helpers
